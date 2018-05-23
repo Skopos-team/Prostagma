94100ae607375e7016d1d74df38eaec58540066d
FilippoPedrazziniFP <filippo.pedrazzini94@gmail.com>
2018-05-23 14:31:32 +0200

    added hold_out and another kind fo cross validation

--- prostagma/performances/cross_validation.py
+++ prostagma/performances/cross_validation.py
@@ -9,28 +9,46 @@ class CrossValidation(object):
 		"""
 		self.k_fold = k_fold
 
+	"""
+		Search for Iterated K-fold validation with shuffling.
+	"""
+
 	def fit(self, X_train, y_train, model):
 
-		num_val_samples = len(train_data) // k 
-		num_epochs = 100
-		all_scores = []
+		all_mae_histories = [] 
 		for i in range(k):
-			print('processing fold #', i)
-			val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples] val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
-		 	
-		 	partial_train_data = np.concatenate( [train_data[:i * num_val_samples],
-				train_data[(i + 1) * num_val_samples:]], axis=0)
-			
-			partial_train_targets = np.concatenate( [train_targets[:i * num_val_samples],
-				train_targets[(i + 1) * num_val_samples:]], axis=0)
-			
-			model = build_model()
-			
-			model.fit(partial_train_data, partial_train_targets, 
-				epochs=num_epochs, batch_size=1, verbose=0)
-			
-			val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
-		 	
-		 	all_scores.append(val_mae)
+		Prepares the validation data: data from partition #k
+		 print('processing fold #', i)
+		val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples] val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+		 partial_train_data = np.concatenate( [train_data[:i * num_val_samples],
+		train_data[(i + 1) * num_val_samples:]], axis=0)
+		Prepares the training data: data from all other partitions
+		  Licensed to <null>
+		Predicting house prices: a regression example
+		89
+		partial_train_targets = np.concatenate( [train_targets[:i * num_val_samples],
+		train_targets[(i + 1) * num_val_samples:]], axis=0)
+		Builds the Keras model (already compiled)
+		 model = build_model()
+		history = model.fit(partial_train_data, partial_train_targets,
+		validation_data=(val_data, val_targets),
+		epochs=num_epochs, batch_size=1, verbose=0) mae_history = history.history['val_mean_absolute_error']
+		  all_mae_histories.append(mae_history)
 		return all_scores
-		
+
+	def another_method():
+		k=4
+		num_validation_samples = len(data) // k
+		np.random.shuffle(data)
+		validation_scores = [] for fold in range(k):
+		Selects the validation- data partition
+		  validation_data = data[num_validation_samples * fold: num_validation_samples * (fold + 1)]
+		training_data = data[:num_validation_samples * fold] + data[num_validation_samples * (fold + 1):]
+		model = get_model()
+		model.train(training_data)
+		validation_score = model.evaluate(validation_data) validation_scores.append(validation_score)
+		validation_score = np.average(validation_scores)
+	Validation score: average of the validation scores of the k folds
+  	model = get_model()
+	model.train(data)
+	test_score = model.evaluate(test_data)
--- prostagma/performances/hold_out.py
+++ prostagma/performances/hold_out.py
@@ -5,4 +5,24 @@ class HoldOut(object):
 	def __init__(self, arg):
 		super(HoldOut, self).__init__()
 		self.arg = arg
+
+	def fit(X_train, y_train):
+		num_validation_samples = 10000
+np.random.shuffle(data)
+Defines the validation set
+   validation_data = data[:num_validation_samples]
+ data = data[num_validation_samples:]
+training_data = data[:]
+Defines the training set
+   model = get_model() Trains a model on the training
+model.train(training_data)
+validation_score = model.evaluate(validation_data)
+# At this point you can tune your model,
+# retrain it, evaluate it, tune it again...
+data, and evaluates it on the validation data
+ model = get_model() Once you’ve tuned your
+model.train(np.concatenate([training_data,
+                            validation_data]))
+test_score = model.evaluate(test_data)
+		return
 		
